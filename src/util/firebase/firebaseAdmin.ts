d8949f353b0e011ca10bcfc73d7511029ad9cfd2
Darren Carlin <dazftw@gmail.com>
2022-11-05 14:55:36 -0400

    updating firebase admind config

--- src/util/firebase/firebaseAdmin.ts
+++ src/util/firebase/firebaseAdmin.ts
@@ -3,7 +3,9 @@ dotenv.config();
 import admin from 'firebase-admin';
 
 const config = {
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, '\n')
+    : undefined,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   projectId: process.env.FIREBASE_PROJECT_ID,
 };
